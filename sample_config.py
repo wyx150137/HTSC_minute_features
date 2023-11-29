cf82ed82fbd287559003cc49aee28f11cf015270
Yassin <wyx150137@icloud.com>
2023-11-29 12:56:24 +0800

    update_the_sample_config_file

--- sample_config.py
+++ sample_config.py
@@ -4,3 +4,11 @@ joint_quant_config = {
     "password" : "password"
 }
 
+# file_path
+
+my_file_path = "D:\\datalib\\"
+raw_save_path = "D:\\datalib\\因子库\\raw_features\\"
+
+begin_date = '2015-01-01'
+
+max_workers = 12
